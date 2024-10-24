696cff98cb9a75cdcfbf4b0c49a5d2965de1fcad
stephen eshelman, objectthink <stephen.n.eshelman@objectthink.com>
2024-10-24 10:18:18 -0400

    Initial Commit

---
+++ xcode-course/xcode_courseApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_courseApp.swift
+//  xcode-course
+//
+//  Created by stephen eshelman on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct xcode_courseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xcode-courseTests/xcode_courseTests.swift
@@ -0,0 +1,16 @@
+//
+//  xcode_courseTests.swift
+//  xcode-courseTests
+//
+//  Created by stephen eshelman on 10/24/24.
+//
+
+import Testing
+
+struct xcode_courseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
